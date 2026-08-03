50628f0395c349b91ef98b388f610c2da066e9b7
bharatreforms <bharatreforms@outlook.com>
2026-08-03 14:29:00 +0530

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const button = document.querySelector("button");
 
+    button.addEventListener("click", function () {
+        alert("Welcome to Bharat Reforms 2047! More content is coming soon.");
+    });
+});
